ebb2b4fc1b2d8aef64c278d38aead02b27b83e49
Turan <turan.abbasli@ailab.az>
2025-11-19 14:41:38 +0400

    train_test_split and model evaluation over test_set

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ from pathlib import Path
 
 from azcharboundary.segmenter import TextSegmenter
 
+def train_test_split(data: list, ratio: int = 0.2) -> tuple[list, list]:
+    """
+    Splits loaded data into train and test sets.
+
+    Args:
+        data (list): list of loaded data
+        ratio (int): ratio of train set over whole set
+
+    Returns:
+        tuple[list, list]: tuple where first item is train_set andn second is test_set
+    """
+    data_len = len(data)
+    splitting_idx = int(data_len * ratio)
+
+    test_set: list = data[:splitting_idx]
+    train_set: list = data[splitting_idx:]
+
+    return train_set, test_set
+
 def demonstrate_basic_usage(data_dir: Path, save_dir: str):
     """Demonstrate basic usage of the CharBoundary library."""
     # Create a segmenter
@@ -19,19 +38,20 @@ def demonstrate_basic_usage(data_dir: Path, save_dir: str):
     with open(data_dir, "r", encoding="utf-8", errors="replace") as f:
         preprocessed_data = [json.loads(line) for line in f]
 
-    training_data = [item["text"] for item in preprocessed_data]
+    datas = [item["text"] for item in preprocessed_data]
+    train_set, test_set = train_test_split(datas)
 
     # Train the segmenter
-    print(f"Training segmenter with {len(training_data)} training data.")
-    t0 = time.time()
+    print(f"Training segmenter with {len(train_set)} training data.")
+    train_start = time.time()
     metrics = segmenter.train(
-        data=training_data,
+        data=train_set,
         model_params={"n_estimators": 512, "max_depth": 64},
         sample_rate=0.001,  # Increase sample rate to get better class balance
-        left_window=5,  # Specify window sizes during training
+        left_window=5,      # Specify window sizes during training
         right_window=5
     )
-    print("Training completed in {:.2f} seconds.".format(time.time() - t0))
+    print("Training completed in {:.2f} seconds.".format(time.time() - train_start))
     
     # Display training metrics
     print(f"Training metrics:")
@@ -40,6 +60,15 @@ def demonstrate_basic_usage(data_dir: Path, save_dir: str):
     print(f"  Boundary precision:     {metrics.get('precision', 0):.4f}")
     print(f"  Boundary recall:        {metrics.get('recall', 0):.4f}")
     print(f"  Boundary F1-score:      {metrics.get('f1_score', 0):.4f}")
+
+    print(f"Evaluation on test set sized {len(test_set)}.")
+    evaluation_start = time.time()
+    test_metrics = segmenter.evaluate(
+        data=test_set,                          
+        max_samples=None,
+    )
+    print("Evaluation completed in {:.2f} seconds.".format(time.time() - evaluation_start))
+    print("Test metrics:", test_metrics)
 
     save_start = time.time()
     segmenter.save(path=save_dir)
