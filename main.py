3c89f77f633629067a87bb8de29a885fee468973
Turan <turan.abbasli@ailab.az>
2025-11-24 11:12:15 +0400

    simpler main training/eval structure

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ from pathlib import Path
 
 from azcharboundary.segmenter import TextSegmenter
 
+def load_data(data_dir: Path) -> list[str]:
+    """
+    Loads data
+    
+    Args:
+        data_dir (Path): path to data
+
+    Returns:
+        (list[str]): list of training datas
+    """
+    print("Loading data!")
+    with open(data_dir, "r", encoding="utf-8", errors="replace") as f:
+        preprocessed_data: list[dict] = [json.loads(line) for line in f]
+
+    datas = [item["input"] for item in preprocessed_data]
+
+    return datas
+
 def train_test_split(data: list, ratio: int = 0.2, shuffle: bool = True) -> tuple[list, list]:
     """
     Splits loaded data into train and test sets.
@@ -32,39 +50,61 @@ def train_test_split(data: list, ratio: int = 0.2, shuffle: bool = True) -> tupl
 
     return train_set, test_set
 
-def demonstrate_basic_usage(data_dir: Path, save_dir: str): 
-    """Demonstrate basic usage of the CharBoundary library."""
-    # Create a segmenter
-    segmenter = TextSegmenter()
+def print_metrics(metrics: dict, name: str = "Process") -> None:
+    """
+    Prints metrics
     
-    print("Loading data!")
-    with open(data_dir, "r", encoding="utf-8", errors="replace") as f:
-        preprocessed_data: list[dict] = [json.loads(line) for line in f]
-
-    datas = [item["input"] for item in preprocessed_data]
-    train_set, test_set = train_test_split(datas, ratio=0.1)
-
-    # Train the segmenter
-    print(f"Training segmenter with {len(train_set)} training data.\n")
-
-    train_start = time.time()
-    metrics = segmenter.train(
-        data=train_set,
-        model_params={"n_estimators": 32, "max_depth": 8},
-        sample_rate=0.001,    # Increase sample rate to get better class balance
-        left_window=5,      # Specify window sizes during training
-        right_window=5
-    )
-    print("Training completed in {:.2f} seconds.".format(time.time() - train_start))
-    
+    Args:
+        metrics (dict): Metrics
+        name (str): name of process metrics belong to. Default is "Process"
+    """
     # Display training metrics
-    print(f"\nTraining metrics:")
+    print(f"\n{name} metrics:")
     print(f"  Overall accuracy:       {metrics.get('accuracy', 0):.4f}")
     print(f"  Boundary accuracy:      {metrics.get('boundary_accuracy', 0):.4f}")
     print(f"  Boundary precision:     {metrics.get('precision', 0):.4f}")
     print(f"  Boundary recall:        {metrics.get('recall', 0):.4f}")
     print(f"  Boundary F1-score:      {metrics.get('f1_score', 0):.4f}")
 
+def save_model(segmenter: TextSegmenter, path: str = "./", serialization_format: str = "treelite") -> None:
+    """
+    Saves model.
+
+    Args:
+        segmenter (TextSegmenter): segmenter instance
+        path (str): path to model. Default to "./
+        serelization_format (str): format of serialization. Default to "treelite"   
+    """
+    save_start = time.time()
+    segmenter.save(path=path, serialization_format=serialization_format)
+    save_end = time.time()
+    print('\nTime took to save model: {:.2f}'.format(save_end-save_start))
+
+def demonstrate_basic_usage(data_dir: Path, save_dir: str): 
+    """Demonstrate basic usage of the CharBoundary library."""
+    # Create a segmenter
+    segmenter = TextSegmenter()
+    
+    datas = load_data(data_dir=data_dir)
+    train_set, test_set = train_test_split(datas, ratio=0.1)
+
+    # Train the segmenter
+    print(f"Training segmenter with {len(train_set)} training data.\n")
+
+    train_start = time.time()
+    training_metrics = segmenter.train(
+        data=train_set,
+        model_params={"n_estimators": 128, "max_depth": 32},
+        sample_rate=0.001,    # Increase sample rate to get better class balance
+        left_window=9,        # Specify window sizes during training
+        right_window=9,
+        threshold=0.5,
+        use_feature_selection=False,
+        feature_selection_threshold=0.01,
+    )
+    print("Training completed in {:.2f} seconds.".format(time.time() - train_start))
+    print_metrics(metrics=training_metrics, name="Training")
+    
     print(f"\nEvaluation on test set sized {len(test_set)}.")
     evaluation_start = time.time()
     evaluation_metrics = segmenter.evaluate(
@@ -72,30 +112,18 @@ def demonstrate_basic_usage(data_dir: Path, save_dir: str):
         max_samples=None,
         sample_rate=0.001
     )
-
-    # Display evaluation metrics
-    print(f"\nEvaluation metrics:")
-    print(f"  Overall accuracy:       {evaluation_metrics.get('accuracy', 0):.4f}")
-    print(f"  Boundary accuracy:      {evaluation_metrics.get('boundary_accuracy', 0):.4f}")
-    print(f"  Boundary precision:     {evaluation_metrics.get('precision', 0):.4f}")
-    print(f"  Boundary recall:        {evaluation_metrics.get('recall', 0):.4f}")
-    print(f"  Boundary F1-score:      {evaluation_metrics.get('f1_score', 0):.4f}")
     print("Evaluation completed in {:.2f} seconds.".format(time.time() - evaluation_start))
-    
+    print_metrics(metrics=evaluation_metrics, name="Evaluation")
 
-    save_start = time.time()
-    segmenter.save(path=save_dir, serialization_format="treelite")
-    save_end = time.time()
-    print('\nTime took to save model: {:.2f}'.format(save_end-save_start))
-
+    save_model(segmenter=segmenter, path=save_dir, serialization_format="treelite")
 
 def main():
     """Run the example script."""
     data_dir = Path(r'azcharboundary\data\train_data_v3_fixed.jsonl')
     save_dir = "azcharboundary/models/model_v1.tl"
 
-    linux_data_dir = Path("azcharboundary/data/train_data_labeled_types_abbr_cleaned_fixed.jsonl")
-    linux_save_dir = "azcharboundary/trained_models/v2/checkpoint.tl"
+    linux_data_dir = Path("azcharboundary/data/final.jsonl")
+    linux_save_dir = "azcharboundary/trained_models/v3/checkpoint.tl"
 
     demonstrate_basic_usage(linux_data_dir, linux_save_dir)
 
