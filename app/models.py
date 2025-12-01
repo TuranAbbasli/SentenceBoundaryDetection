d26bc10c62f49e1db3e4e1d9081383c3e94555b8
baghirlimurad <murad.baghirli@ailab.az>
2025-12-01 10:51:15 -0500

    Modified api response format

--- app/models.py
+++ app/models.py
@@ -2,6 +2,5 @@ from pydantic import BaseModel
 
 
 class SegmentationResponse(BaseModel):
-    original_text: str
-    segmented_text: str
-    num_boundaries: int
+    sentences: list[str]
+    num_sentences: int
